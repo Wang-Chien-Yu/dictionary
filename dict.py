61dfd8f720beaeabec643f143215f9dfc111d6fb
unknown <josh12365@gmail.com>
2022-05-02 15:43:17 +0800

    add function

--- dict.py
+++ dict.py
@@ -1,39 +1,49 @@
 #讀取檔案
-data = []
-count = 0
-with open('reviews.txt', 'r') as f:
-	for line in f:
-		data.append(line)
-		count +=1
-		#if count % 1000 == 0: # %求餘數
-			#print(len(data))
-print('檔案讀取完了,總共有', len(data), '筆資料')
+def read(filename):
+	data = []
+	with open(filename, 'r') as f:
+		for line in f:
+			data.append(line)
+	print('檔案讀取完了,總共有', len(data), '筆資料')
+	return data
 
-wc = {} #word_count 
 
-for d in data:
-	words = d.split()
-	for word in words:
+def add_dict(data):
+	wc = {} #word_count 
+	for d in data:
+		words = d.split()
+		for word in words:
+			if word in wc:
+				wc[word] += 1 #如果有value +1
+			else:	
+				wc[word] = 1 #如果沒有新增key進wc字典
+	return wc
+
+
+def  print_msg(wc):
+	for w in wc:
+		if wc[w] > 100:
+			print(w, wc[w])
+
+
+def user_input(wc):
+	while True:
+		word = input('請輸入想查詢的字: ')
+		if word == 'q':
+			print('感謝你的查詢')
+			break
 		if word in wc:
-			wc[word] += 1 #如果有value +1
-		else:	
-			wc[word] = 1 #如果沒有新增key進wc字典
-
-# for w in wc:
-# 	if wc[w] > 100:
-# 		print(w, wc[w])
-
-while True:
-	word = input('請輸入想查詢的字: ')
-	if word == 'q':
-		print('感謝你的查詢')
-		break
-	if word in wc:
-		print(word, '出現過的次數為', wc[word])
-	else:
-		print('這個字沒有出現過')
+			print(word, '出現過的次數為', wc[word])
+		else:
+			print('這個字沒有出現過')
 
 
+def main():
+	data = read('reviews.txt')
+	wc = add_dict(data)
+	user_input(wc)
+
+main()
 # #dictionary 字典
 # #words = {
 # # 	'key': 'value'
